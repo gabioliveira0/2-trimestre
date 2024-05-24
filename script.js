82a5b9ce5266c9819dcaa693bcf7e9d6a7faf8e7
gabioliveira0 <162706091+gabioliveira0@users.noreply.github.com>
2024-05-24 10:25:43 -0300

    a

--- script.js
+++ script.js
@@ -22,13 +22,11 @@ const perguntas = [
       enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
       alternativas: [
         {
-          texto:
-            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+          texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
           afirmacao: "afirmação",
         },
         {
-          texto:
-            "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+          texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
           afirmacao: "afirmação",
         },
       ],
@@ -37,13 +35,11 @@ const perguntas = [
       enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
       alternativas: [
         {
-          texto:
-            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+          texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
           afirmacao: "afirmação",
         },
         {
-          texto:
-            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+          texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
           afirmacao: "afirmação",
         },
       ],
@@ -52,8 +48,7 @@ const perguntas = [
       enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
       alternativas: [
         {
-          texto:
-            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+          texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
           afirmacao: "afirmação",
         },
         {
@@ -66,13 +61,11 @@ const perguntas = [
       enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
       alternativas: [
         {
-          texto:
-            "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+          texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
           afirmacao: "afirmação",
         },
         {
-          texto:
-            "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+          texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
           afirmacao: "afirmação",
         },
       ],
