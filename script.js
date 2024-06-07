dccbac23b074e0f74de6ed149e85a887bc985dfa
gabioliveira0 <162706091+gabioliveira0@users.noreply.github.com>
2024-06-07 09:44:19 -0300

    b

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ function mostraPergunta(){
     for(const alternativa of perguntaAtual.alternativas){
             const botaoAlternativas = document.createElement("button");
             botaoAlternativas.textContent = alternativa.texto;
-            botaoAlternativas.addEventListener("click", function(){
+            botaoAlternativas.addEventListener("click", () => respostaSelecionada()){
                 atual++;
                 mostraPergunta();
             })
