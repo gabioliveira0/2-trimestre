9098467cbd1a1d8823730701a9550394135950a6
gabioliveira0 <162706091+gabioliveira0@users.noreply.github.com>
2024-06-07 10:25:25 -0300

    a

--- script.js
+++ script.js
@@ -90,12 +90,16 @@ function mostraPergunta(){
     for(const alternativa of perguntaAtual.alternativas){
             const botaoAlternativas = document.createElement("button");
             botaoAlternativas.textContent = alternativa.texto;
-            botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));{
-                atual++;
-                mostraPergunta();
-            }
+            botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
             caixaAlternativas.appendChild(botaoAlternativas);
     }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++;
+    mostraPergunta();
 }
 
 mostraPergunta();
