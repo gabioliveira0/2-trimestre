606a1dcf3582c9a6fcd013131b0fbeacad116728
gabioliveira0 <162706091+gabioliveira0@users.noreply.github.com>
2024-06-07 10:45:56 -0300

    a

--- script.js
+++ script.js
@@ -96,10 +96,7 @@ function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
             const botaoAlternativas = document.createElement("button");
             botaoAlternativas.textContent = alternativa.texto;
-            botaoAlternativas.addEventListener("click", function(){
-                atual++;
-                mostraPergunta();
-            })
+            botaoAlternativas.addEventListener("click", function();
             caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
