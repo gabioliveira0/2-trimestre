df4b9b85f642d868419d3ba84b8b6d6d97e80a8b
gabioliveira0 <162706091+gabioliveira0@users.noreply.github.com>
2024-06-07 10:49:48 -0300

    c

--- script.js
+++ script.js
@@ -103,9 +103,15 @@ function mostraAlternativas(){
 
     function respostaSelecionada(opcaoSelecionada){
         const afirmacoes = opcaoSelecionada.afirmacoes;
-        historiaFinal = afirmacoes;
+        historiaFinal += afirmacoes + "";
         atual++;
         mostraPergunta();
+    }
+
+    function mostraResultado() {
+        caixaPerguntas.textContent = "Em 2049...";
+        textoResultado.textContent = historiaFinal;
+        caixaAlternativas.textContent = "";
     }
 
     mostraPergunta();
